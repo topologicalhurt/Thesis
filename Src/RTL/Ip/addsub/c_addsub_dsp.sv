57bf6cdf6b4343c122cc489cefe6b2cc914b7cb4
topologicalhurt <csin0659@uni.sydney.edu.au>
2025-05-08 23:17:48 +1000

    WIP: rectify this

---
+++ Src/RTL/Ip/addsub/c_addsub_dsp.sv
@@ -0,0 +1,7 @@
+module c_addsub_dsp (
+	input wire CLK, CE, SCLR,
+	input wire [23:0] A, B,
+	output wire [24:0] S
+);
+
+endmodule
---
+++ Src/RTL/Ip/cordicvec/cordic_1.sv
@@ -0,0 +1,8 @@
+module cordic_1 (
+	input wire aclk, s_axis_cartesian_tvalid,
+	input wire [47:0] s_axis_cartesian_tdata,
+	output wire m_axis_dout_tvalid,
+	output wire [47:0] m_axis_dout_tdata
+);
+
+endmodule
