26f624d1abf9160420960954d1ecfe1cf4296b85
topologicalhurt <csin0659@uni.sydney.edu.au>
2025-09-10 00:20:29 +1000

    Update: headers

--- Src/Allocator/Interpreter/tests/polynomial_cse_iccad_test.py
+++ Src/Allocator/Interpreter/tests/polynomial_cse_iccad_test.py
@@ -1,6 +1,31 @@
 """
-Test module for ICCAD 2004 paper-based polynomial CSE implementation.
+------------------------------------------------------------------------
+Filename: 	polynomial_cse_iccad_test.py
+
+Project:	LLAC, intelligent hardware scheduler targeting common audio signal chains.
+
+For more information see the repository: https://github.com/topologicalhurt/Thesis
+
+Purpose:	Test module for ICCAD 2004 paper-based polynomial CSE implementation.
 Tests the exact algorithms from the paper: kernel extraction, KCM formation, and optimization.
+
+Author: topologicalhurt csin0659@uni.sydney.edu.au
+
+------------------------------------------------------------------------
+Copyright (C) 2025, LLAC project LLC
+
+This file is a part of the ALLOCATOR module
+It is intended to be used as part of the allocator design which is responsible for the soft-core, or offboard, management of the on-fabric components.
+Please refer to docs/whitepaper first, which provides a complete description of the project & it's motivations.
+
+The design is NOT COVERED UNDER ANY WARRANTY.
+
+LICENSE:     GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+As defined by GNU GPL 3.0 https://www.gnu.org/licenses/gpl-3.0.html
+
+A copy of this license is included at the root directory. It should've been provided to you
+Otherwise please consult: https://github.com/topologicalhurt/Thesis/blob/main/LICENSE
+------------------------------------------------------------------------
 """
 import math
 import numpy as np
